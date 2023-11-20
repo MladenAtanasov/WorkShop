85b95fd418b90bbe46a594f1b393e69a28f7e9e6
Mladen Atanasov <mladenbiserov@abv.bg>
2023-11-21 01:26:58 +0200

    Add game service

--- client/src/components/gameCreate/GameCreate.jsx
+++ client/src/components/gameCreate/GameCreate.jsx
@@ -1,7 +1,19 @@
+import {create} from '../../services/gameService'
+
 export const GameCreate = () => {
+	const createGameSubmitHandler = async (e) => {
+		e.preventDefault()
+		
+		const gameData = Object.fromEntries(new FormData(e.currentTarget));
+		
+		const result = await create(gameData);
+		
+		console.log(result);
+	}
+	
     return (
         <section id="create-page" className="auth">
-            <form id="create">
+            <form id="create" onSubmit={createGameSubmitHandler}>
                 <div className="container">
                     <h1>Create Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
